c1374888d6ebcfc31858c7c74b2431c0a3efaf0b
zeitpop, via DB node <alexkoure@gmail.com>
2021-06-04 20:43:38 +0000

    minor updates, consolidating package requirements

--- bin/insert_data.py
+++ bin/insert_data.py
@@ -2,6 +2,7 @@ import json
 from sqlalchemy import *
 from sqlalchemy import table, create_engine
 import sys
+import requests
 
 ####################
 ## Initialize 
@@ -10,7 +11,7 @@ import sys
 debug = 1 
 
 ## Load .json file of data
-with open("../data/test_oracle.json") as d:
+with open("../data/bulk_data.json") as d:
     data = json.load(d)
 
 ## Create handle to database
@@ -34,23 +35,8 @@ card_faces = Table('card_faces', meta, autoload_with=engine)
 # Create list of columns, ideally we replace this by pulling column names from table objects metadata    
 columns = ["internal_card_id", "scryfall_card_id", "arena_id", "lang", "object", "oracle_id", "scryfall_uri", "uri", "card_face_id", "cmc", "color_identity", "color_indicator", "colors", "layout", "loyalty", "mana_cost", "name", "oracle_text", "power", "produced_mana", "toughness", "type_line", "flavor_text", "rarity", "release_at", "reprint", "set_name", "set_type", "set_code"]
 
-###### Test zone
 
-test_loop = 'disabled'
-if test_loop == 'enabled':
-    
-    for raw_card in data:
-        processed_card = dict.fromkeys(columns)
-        print(raw_card['name'])
-        
-        for key in processed_card:
-            if key in raw_card and type(raw_card[key]) is list:
-                print("value of", raw_card['name'], ", key ", key, " is a list")
-
-    sys.exit("terminating early for testing")
-
-####### Begin real zone
-
+# Iterate through cards in raw data, transform into processed dicts for insertion
 for raw_card in data:
     
     processed_card = dict.fromkeys(columns)
@@ -60,53 +46,19 @@ for raw_card in data:
     for key in processed_card:
     
         if debug == 2: print("\tSetting key: ", key)
+
+
+        # Assign Key Values
+        if key == 'scryfall_card_id': processed_card[key] = raw_card['id']
+        elif key == 'set_code': processed_card[key] = raw_card['set']    
+        elif key == 'card_face': processed_card[key] = 'NULL'
+        elif key in raw_card and type(raw_card[key]) == list: processed_card[key] = ''.join(map(str, raw_card[key])) 
+        elif key in raw_card: processed_card[key] = raw_card[key]
     
-        # if key is
-        if key == 'scryfall_card_id':
-            processed_card[key] = raw_card['id']
-
-        # otherwise, if key is
-        elif key == 'set_code':
-            processed_card[key] = raw_card['set']
-        
-        # otherwise if key is
-        elif key == 'card_face':
-            if debug == 3: print("\t\t\tkey == card_face, setting as NULL")
-            # don't set? leave blank? don't have auto-increment setup
-            processed_card[key] = 'NULL'
-       
-        # otherwise if key is
-        elif key in raw_card and type(raw_card[key]) == list:
-            if debug == 3: print("\t\t\tkey is a list")
-
-            processed_card[key] = ''.join(map(str, raw_card[key]))
-        
-
-        #elif key == 'produced_mana':
-        #    if debug == 2: print("\tkey == ", key)
-        #    if key in raw_card:
-        #        # stringify and store
-        #        print("\t\tproduced_mana is a key of ", raw_card['name'], ", inserting value: ")
-        #        print("\t\t\t", raw_card[key])
-        #        processed_card[key] = ''.join(map(str, raw_card[key]))
-        #
-        #    else:
-        #        if debug == 2: print("\t\tproduced_mana is NOT a key of ", raw_card['name'], ", inserting null value")
-        #        processed_card[key] = 'NULL' 
-
-        # otherwise if key to fill from proc_card exists within raw_card, pass it directly:
-        elif key in raw_card:
-           # if debug == 2: print("\t\t ", key, " <-- ", raw_card[key])
-            processed_card[key] = raw_card[key]
         if debug == 2: print("\t\t Type: ", type(processed_card[key]), "\n\t\tValue: ", processed_card[key])
  
     if debug == 1 or 2:
         print("\n Processed Card:", processed_card['name'], "\n")
-
-        #for key in processed_card:
-            #print("\tKey: ", key)
-            #print("\t\tType: ", type(processed_card[key]))
-            #print("\t\tValue: ", processed_card[key])
 
     statement = insert(cards).values(processed_card)
         
