d90111504473e96e82daa36390760d332cb398ef
zeitpop, via DB node <alexkoure@gmail.com>
2021-06-08 16:00:44 +0000

    scrape now IDs maindeck vs sideboard, ignores column headers

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -27,12 +27,44 @@ parsed_html = bs4.BeautifulSoup(html_file, 'html.parser')
     # haven't figured out how to step through the results. 
     # Missing something about how .find() vs .find_all() work
 
+
+# maindeck_results = parsed_html.find_all('div', class_='O14', string=[re.compile('LANDS'), re.compile('CREATURES'), re.compile('INSTANTS'), re.compile('OTHER SPELLS')])
+print("Main Deck: ")
+
+# Find Column Header divs 
+for category in parsed_html.find_all('div', class_='O14', string=[re.compile('LANDS'), re.compile('CREATURES')]):
+
+    # Column headers are siblings of card divs, so find iterate through siblings
+    for card in category.find_next_siblings():
+        # test checking for a category header
+        if card.get_text() == card.find(string=re.compile('OTHER SPELLS')) or card.get_text() == card.find(string=re.compile('INSTANTS and SORC')):
+            print("  ", card.get_text(), " (Skipping)")
+        else: 
+            print("\t", card.get_text())
+
+        
+        
+
+        # print("\t", card.get_text(), " ", type(card), card)
+        
+
+#print("maindeck_results Type: ", type(maindeck_results))
+#print("maindeck_results: ", maindeck_results)
+#for element in maindeck_results:
+  
+#    for card in element.find_next_siblings():
+        #print("card in element.find_next_siblings() Type: ", type(card))
+#        print(card.get_text())
+
+# Creatures
+
 # Finds Sideboard div
-results = parsed_html.find('div', class_='O14', string='SIDEBOARD').find_next_siblings()
+sideboard_results = parsed_html.find('div', class_='O14', string='SIDEBOARD').find_next_siblings()
 
 
-for div in results:
-   print(div.get_text())
+print("Sideboard: ")
+for div in sideboard_results:
+   print("\t", div.get_text())
 
 ########## ( Works ) 
 # (Works) For now, we won't differentiate sideboard and just get all cards:
