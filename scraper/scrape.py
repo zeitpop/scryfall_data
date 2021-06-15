f897e56e701f4c79eafde13e126f30176a9536b3
zeitpop, via DB node <alexkoure@gmail.com>
2021-06-15 17:14:36 +0000

    event/deck data now get formatted for submit to db

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -5,7 +5,6 @@ from sqlalchemy import *
 ####################
 # Initialization
 
-
 deck_metadata = {}
 main_deck = {}
 sideboard = {}
@@ -14,8 +13,41 @@ sideboard = {}
 print_maindeck = False
 print_sideboard = False
 print_skipped_column_headers = False
+print_event_data = True
+print_deck_data = True
+
+# To be deprecated?
 print_metadata = False
 
+
+### Init Database
+
+# Import database credentials
+load_dotenv()
+database_url = os.environ.get('DATABASE_URL')
+database_username = os.environ.get('DATABASE_USERNAME')
+database_password = os.environ.get('DATABASE_PASSWORD')
+
+# Create connection string and engine object
+connection_string = ("mysql+mysqldb://" + database_username + ":" + database_password + "@" + database_url + "/scryfall")
+
+engine = create_engine(connection_string, echo=False, future=True, pool_pre_ping=True)
+
+# Create MetaData object
+meta = MetaData()
+
+# Construct Table objects
+events = Table('events', meta, autoload_with=engine)
+decks = Table('decks', meta, autoload_with=engine)
+decklists = Table('decklists', meta, autoload_with=engine)
+
+# Instantiate objects to receive processed data
+
+event_data = dict.fromkeys([c.name for c in events.columns])
+deck_data = dict.fromkeys([c.name for c in decks.columns])
+
+#############################################################
+# Parse HTML bs4 object for decklist, sideboard, metadata
 
 # Open File and read
 with open ("test_page.html", "r") as raw_file:
@@ -24,28 +56,7 @@ with open ("test_page.html", "r") as raw_file:
 # Parse html into a bs4 object
 parsed_html = bs4.BeautifulSoup(html_file, 'html.parser')
 
-    # Tree Structure of parsed .html file: 
-        # 3 divs for each column
-    #   <div style="margin:3px;flex:1;" align="left">
-    #       
-    #       # Divs to section card types within column 
-    #       <div class="O14">10 LANDS</div>
-            
-            # divs for card name and count within section (siblings of section divs) 
-    #       <div id="mdara009" class="deck_line chosen_tr" ..4 .. Thoughtseize .. </div>
-    #       ...
-    #       <div id="mdara009" class="deck_line chosen_tr" ..2 .. Ruin Crab.. </div>
-    #   </div>
-    # 
-    # Note: Lands and Sideboard are own column, creatures, instants, sorcs and others share middle column
-
-
-
-#############################################################
-# Parse HTML bs4 object for decklist, sideboard, metadata
-
-
-### Parse HTML for Main Deck cards and store in main_deck dict
+# ----- MAIN DECK -----
 
 # Find Column Header divs for main deck.
 for category in parsed_html.find_all('div', class_='O14', string=[re.compile('LANDS'), re.compile('CREATURES')]):
@@ -68,7 +79,7 @@ if print_maindeck == True:
 
 
 
-### Parse for Sideboard and populate sideboard dict
+# ---- SIDEBOARD -----
 
 # Finds Sideboard div
 sideboard_results = parsed_html.find('div', class_='O14', string='SIDEBOARD').find_next_siblings()
@@ -85,8 +96,9 @@ if print_sideboard == True:
     for key in sideboard: print("\t", sideboard[key], " ", key)
 
 
-### Parse for metadata and populate deck_metadata dict
+# ----- METADATA ------
 
+# Create parsed .html object
 metadata_results = parsed_html.find_all('div', class_='event_title')
 
 
@@ -94,12 +106,20 @@ metadata_results = parsed_html.find_all('div', class_='event_title')
 place_title_author = metadata_results[1].get_text().split(' - ', 1)
 place_title = place_title_author[0].split(" ", 1)
 
-# Insert into metadata dict
-deck_metadata['Event'] = metadata_results[0].get_text()
-deck_metadata['Author'] = place_title_author[1]
-deck_metadata['Placement'] = place_title[0]
-deck_metadata['Deck Title'] = place_title[1]
-deck_metadata['Format'] = parsed_html.find('div', class_='meta_arch').get_text()
+ # deck_data['event_id'] = how to get event_id after data gets submitted?
+deck_data['deck_title'] = place_title[1]
+deck_data['deck_format'] = parsed_html.find('div', class_='meta_arch').get_text()
+deck_data['placement'] = place_title[0]
+deck_data['deck_author'] = place_title_author[1]
+
+# ------- to be deprecated --------- #
+# Insert into metadata dict (to be deprecated)
+
+# deck_metadata['Author'] = place_title_author[1]
+# deck_metadata['Placement'] = place_titlei[0]
+# deck_metadata['Deck Title'] = place_title[1]
+# deck_metadata['Format'] = parsed_html.find('div', class_='meta_arch').get_text()
+# --------------------------------------------
 
 
 # Extract other metadata (Event Date, Event Link)
@@ -113,9 +133,11 @@ for i in date_results.next_siblings:
     if isinstance(i, bs4.element.Tag):
         date_text_results.append(i.get_text())
 
-
-deck_metadata['event_link'] = date_text_results[1]
-deck_metadata['event_date'] = date_text_results[0].split(' - ', 1)[1]
+# Set event data
+event_data['event_name'] = metadata_results[0].get_text()
+event_data['event_date'] = date_text_results[0].split(' - ', 1)[1]
+event_data['event_format'] = parsed_html.find('div', class_='meta_arch').get_text()
+event_data['event_link'] = date_text_results[1]
 
 if print_metadata == True: 
     print("Metadata:")
@@ -126,44 +148,47 @@ if print_metadata == True:
 ####################################################################
 # Load Extracted Data into Database
 
-### Init Database
+
+# ---- Event Data ----
+
+if print_event_data == True: 
+    print("Event data: ")
+    for key in event_data: print("\t", key, ": ", event_data[key])
+
+# Check if event is in database:
+# if checkedEventInDatabase == False:
+    # event_query_results = select * FROM scryfall.events WHERE event_date == ... AND event_name == ...
+    # if event_query_results == None:
+        # checkedEventInDatabase == True
+        # eventInDatabase == False
+    # elseif event_query_results =! None:
+        # checkedEventInDatabase == True
+        # eventInDatabase ==True
+
+# if eventInDatabase == False:
+    # event_statement = insert(events).values(event_data)
+# elseif eventInDatabase:
+    # deck_data['event_id'] = event_query_results['event_id']
 
 
-# Import database credentials
-load_dotenv()
-database_url = os.environ.get('DATABASE_URL')
-database_username = os.environ.get('DATABASE_USERNAME')
-database_password = os.environ.get('DATABASE_PASSWORD')
 
-# Create connection string and engine object
-connection_string = ("mysql+mysqldb://" + database_username + ":" + database_password + "@" + database_url + "/scryfall")
+# ---- Deck Data ----
 
-engine = create_engine(connection_string, echo=False, future=True, pool_pre_ping=True)
+if print_deck_data == True:
+    print("Deck Data: ")
+    for key in deck_data: print("\t", key, ": ", deck_data[key])
+# if deck is not in database:
+#    deck_statement = insert(decks).values(deck_data)
+# else:
+#   get deck_id
 
-# Create MetaData object
-meta = MetaData()
 
-# Construct Table objects
-events = Table('events', meta, autoload_with=engine)
-decks = Table('decks', meta, autoload_with=engine)
-decklists = Table('decklists', meta, autoload_with=engine)
-
-### Event Data
-
-# 
-for c in events.columns:
-    print(c.name)
-
-# Check if event is already in database and either
-    # Get event ID
-    # Load Event data into database
-
-### Deck Data
-
-# Check if deck is already in database
-# Load deck into database
-
-### Decklist Data
+# ---- Decklist Data ----
 
 # Load decklist into database
+
+# For item in decklist, item =cardname, item(key) = count_maindeck
+# For item in sideboard, item = cardname, item(key) = count_sideboard
+# not this --> decklist_statement = insert(deckilsts).values(decklists_data)
+
 
