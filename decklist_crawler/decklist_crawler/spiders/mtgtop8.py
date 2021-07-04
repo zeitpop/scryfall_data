7682533c537f13abacfe1e4878efd3199159dce5
akoure <alexkoure@gmail.com>
2021-07-04 10:21:36 -0400

    adding database connection to parse

--- decklist_crawler/decklist_crawler/spiders/mtgtop8.py
+++ decklist_crawler/decklist_crawler/spiders/mtgtop8.py
@@ -43,13 +43,33 @@ class Mtgtop8Spider(CrawlSpider):
         main_deck = {}
         sideboard = {}
         
-        # Somehow load objects from table
+        ## Create database engine object
 
-        #event_data = 
-        #deck_data = 
-        #decklist_data =
+        load_dotenv()
 
-        item = scrapy.Item()
+        database_url = os.environ.get('DATABASE_URL')
+        database_username = os.environ.get('DATABASE_USERNAME')
+        database_password = os.environ.get('DATABASE_PASSWORD')
+        
+        connection_string = ("mysql+mysqldb://" + database_username + ":" + database_password + "@" + database_url + "/scryfall")
+
+        engine = create_engine(connection_string, echo=False, future=True, pool_pre_ping=True)
+
+        # Create MetaData object
+        meta = MetaData()
+
+        # Construct Table objects
+        events = Table('events', meta, autoload_with=engine)
+        decks = Table('decks', meta, autoload_with=engine)
+        decklists = Table('decklists', meta, autoload_with=engine)
+        cards = Table('cards', meta, autoload_with=engine)
+
+        # Instantiate objects to receive processed data
+        event_data = dict.fromkeys([c.name for c in events.columns])
+        deck_data = dict.fromkeys([c.name for c in decks.columns])
+        decklist_data = dict.fromkeys([c.name for c in decklists.columns])
+
+        #item = scrapy.Item()
 
         
         # Gotta insert all the beautiful soup extraction stuff here
