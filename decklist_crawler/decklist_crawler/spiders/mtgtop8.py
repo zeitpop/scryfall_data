65d962841bd9cf166d7e70fbbad39eb6eadd5ad3
zeitpop <alexkoure@gmail.com>
2021-07-08 17:50:08 -0400

    crawler now parses decklist and accounts for inconsistent column divs

--- decklist_crawler/decklist_crawler/spiders/mtgtop8.py
+++ decklist_crawler/decklist_crawler/spiders/mtgtop8.py
@@ -19,40 +19,37 @@ class Mtgtop8Spider(CrawlSpider):
 
     rules = (
 
-        # Extract Links matching the deck URL pattern and extract deck items
+        # Allow the main indicator of a page with a deck, deny the visual view, set callback function
         Rule(LinkExtractor(allow=('mtgtop8.com/event?', ), deny=('&switch=visual')), callback='parse_decklist'),
 
+        # Deny a few link categories that get appear early in pages but don't yield many pages with decks
         Rule(LinkExtractor(deny=('/topcards?', '/archetype?' ))),  
 
-        # Non-deck sites
+        # Set callback function for all other (non-deck) pages
         Rule(LinkExtractor(), callback = 'parse'),
 
     )
 
+    # Callback Function for pages with decklists
     def parse_decklist(self, response):
         
         # Debugging
         
-        debug_parse_maindeck = False
-        print_maindeck = True
-        print_skipped_column_headers = True
+        debug_parse_decklist = False
+        debug_parse_metadata = True
+        print_parsed_decklist = True
 
-        print_sideboard = False
-        
-        print_event_data = False
-        print_deck_data = False
+        print_event_data = True
+        print_deck_data = True
+
+        extract_metadata = False
+
         print_decklist_data = False
         print_database = False
         
         exec_without_commit = False
 
         load_database = False
-
-        extract_main_deck = False
-        extract_sideboard = False
-        extract_metadata = False
-
-        procedural_crawl = False
 
         # Instantiate
         main_deck = {}
@@ -97,32 +94,16 @@ class Mtgtop8Spider(CrawlSpider):
         #############################################################
         # Parse HTML bs4 object for decklist, sideboard, metadata
 
-        # Open File and read
-        #with open ("test_page.html", "r") as raw_file:
-        #    html_file=raw_file.read()
-
         # Parse html into a bs4 object
-        #parsed_html = bs4.BeautifulSoup(html_file, 'html.parser')
 
+        self.logger.info('Parsing response...')
         parsed_html = bs4.BeautifulSoup(response.text, 'html.parser')
         
 
-        # Test zone for making new method for extracting deck / sideboard
-            # Problem is there are 3 columns, with child divs of section headers and cards at same level.
-            # Depending on how many cards in columns, distribution of sections will vary between the 3 columns
-            # Probably need to combine contents of each column into a single navigable string, then parse accordingly
+        # ----- EXTRACT DECKLIST ------ #
 
-        # Tentative alternative method of parsing decklist:
-
-            # Get 3 columns into a resultSet
-            # Take contents of each column and append to single navigable string object
-            # Iterate through single NavigableSTring, skipping column headers and switching to sideboard once past that header
-
-
-########### BLOCK TO REPLACE OLD MAINDECK AND SIDEBOARD PARSER
-
+        # Create NavigableString object containing the 3 columns with headers and cards
         div = parsed_html.find('div', class_='O14', string=re.compile('LANDS'))
-
         columns = div.parent.parent
 
         maindeck_column_names = ['LANDS', 'CREATURES', 'INSTANTS and SORC.', 'OTHER SPELLS']
@@ -137,11 +118,11 @@ class Mtgtop8Spider(CrawlSpider):
 
                 if any(item in maindeck_column_names for item in text):
                     
-                    if debug_parse_maindeck == True: print(f'COLUMN NAME: {text}')
+                    if debug_parse_decklist == True: self.logger.info(f'COLUMN NAME: {text}')
                 
                 elif any(item in ['SIDEBOARD'] for item in text):
                     
-                    if debug_parse_maindeck == True: print(f'SIDEBOARD: {text}')
+                    if debug_parse_decklist == True: self.logger.info(f'SIDEBOARD: {text}')
                     target_sideboard = True
 
                 else:
@@ -156,77 +137,15 @@ class Mtgtop8Spider(CrawlSpider):
                     else:
                         main_deck[cardname] = count
 
-                    if debug_parse_maindeck == True: print(f'Count: {count}, Cardname: {cardname}')
+                    if debug_parse_decklist == True: self.logger.info(f'Count: {count}, Cardname: {cardname}')
 
-        #self.logger.info(f'Found div of type {type(div)} with length {len(div)}')
-        #for item in div:
-        #    self.logger.info(f'\tFound item in div with contents:\n{item.get_text()}')
+           
+            if print_parsed_decklist == True:
+                self.logger.info("Main Deck: ")
+                for key in main_deck: self.logger.info(f'\t {main_deck[key]} {key}')
+                self.logger.info("Sideboard: ")
+                for key in sideboard: self.logger.info(f'\t {sideboard[key]} {key}')
 
-        # self.logger.info(f'\n\n Printing test div: \n{div.prettify()}')
-
-
-
-        # ----- ETRACT MAIN DECK -----
-        
-
-        if extract_main_deck == True:
-            
-
-            ###### OLD CODE FOR MAINDECK ###########            
-            if debug_parse_maindeck == True: self.logger.info('Parsing Main Deck...')
-
-            # Find Column Header divs for main deck.
-            main_deck_html = parsed_html.find_all('div', class_='O14', string=[re.compile('LANDS'), re.compile('CREATURES')])
-
-            if print_maindeck== True: self.logger.info(f'Received main_deck_html with type {type(main_deck_html)} and {len(main_deck_html)} items')
-
-            # Iterate through divs containing main deck
-            for category in main_deck_html:
-
-                if debug_parse_maindeck == True: self.logger.info(f'Iteration of main_deck_html\n\t{category}')
-                
-                # Column headers are siblings of card divs, so find & iterate through siblings
-                for card in category.find_next_siblings():
-                    
-                    # Check if div is just a column header. Skip or add to deck accordingly
-                    if card.get_text() == card.find(string=re.compile('OTHER SPELLS')) or \
-                       card.get_text() == card.find(string=re.compile('INSTANTS and SORC')) or \
-                       card.get_text() == card.find(string=re.compile('CREATURES')):
-                           if debug_parse_maindeck == True:
-                               self.logger.info(f'\t {card.get_text()} skipping...')
-                    else:
-                        if debug_parse_maindeck == True: self.logger.info(f'\t {card.get_text()}')
-                        
-                        # Get text of div contaiing card and add to main_deck
-                        text = card.get_text().split(" ", 1)
-                        main_deck[text[1]] = text[0]
-            
-            if debug_parse_maindeck == True: self.logger.info(f'Completed extracting Main Deck')
-
-
-
-            if print_maindeck == True:
-                self.logger.info('Main Deck Contents: ')
-                for key in main_deck: 
-                    self.logger.info(f'\t{main_deck[key]} {key}')
-
-        # ---- EXTRACT SIDEBOARD -----
-        
-        if extract_sideboard == True:
-
-            # Finds Sideboard div
-            sideboard_results = parsed_html.find('div', class_='O14', string='SIDEBOARD').find_next_siblings()
-            
-            # Iterate through tags in results for sideboard cards
-            for div in sideboard_results:
-               # print("\t", div.get_text())
-               sb_text = div.get_text().split(" ", 1)
-            
-               sideboard[sb_text[1]] = sb_text[0]
-            
-            if print_sideboard == True:
-                print("Sideboard: ")
-                for key in sideboard: print("\t", sideboard[key], " ", key)
 
         # ----- EXTRACT METADATA ------
 
@@ -234,11 +153,14 @@ class Mtgtop8Spider(CrawlSpider):
 
             # Create parsed .html object
             metadata_results = parsed_html.find_all('div', class_='event_title')
-            
+            if debug_parse_metadata == True: self.logger.info(f'\nmetadata_results:\n{metadata_results}')
             
             # Extract Deck Author, Title, and Placement in Event
             place_title_author = metadata_results[1].get_text().split(' - ', 1)
+            if debug_parse_metadata == True: self.logger.info(f'\nplace_title_author:\n{place_title_author}')
+
             place_title = place_title_author[0].split(" ", 1)
+            if debug_parse_metadata == True: self.logger.info(f'\nplace_title:\n{place_title}')
             
             # deck_data['event_id'] = how to get event_id after data gets submitted?
             deck_data['deck_title'] = place_title[1]
@@ -246,6 +168,10 @@ class Mtgtop8Spider(CrawlSpider):
             deck_data['placement'] = place_title[0]
             deck_data['deck_author'] = place_title_author[1]
             
+            if print_deck_data == True:
+                for key in deck_data:
+                    self.logger.info(f'{key} {deck_data[key]}')
+
             # Extract other metadata (Event Date, Event Link)
             date_results = parsed_html.find('div', class_='meta_arch')
             
@@ -263,19 +189,22 @@ class Mtgtop8Spider(CrawlSpider):
             event_data['event_format'] = parsed_html.find('div', class_='meta_arch').get_text()
             event_data['event_link'] = date_text_results[1]
             
-        ###############################################################
-        # 
+
+            if print_event_data == True:
+                for key in event_data:
+                    self.logger.info(f'{key} {event_data[key]}')            
 
         # item = [main_deck, sideboard, deck_data, event_data]
 
         # yield item
 
-        if procedural_crawl == True:
+        # Find links and yield request objects to engine
+        for href in response.xpath('//a/@href').getall():
+          yield scrapy.Request(response.urljoin(href), self.parse)
 
-            for href in response.xpath('//a/@href').getall():
-              yield scrapy.Request(response.urljoin(href), self.parse)
-
+    # Callback Function for pages without decklist
     def parse(self, response):
 
+        # Find links and yiel request objects to engine
         for href in response.xpath('//a/@href').getall():
             yield scrapy.Request(response.urljoin(href))
